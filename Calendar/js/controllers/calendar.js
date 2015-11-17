3dc55171dbff3bf39ad4a05aaf0faf192de3ef48
Oleg Pylypchak <liole.plo@gmail.com>
2015-11-18 00:45:23 +0200

    Profile selector, grid layout

--- Calendar/js/controllers/calendar.js
+++ Calendar/js/controllers/calendar.js
@@ -1,5 +1,26 @@
 ﻿angular.module('calendar')
 .controller('calendar', function ($scope, $routeParams, repository) {
 
-    $scope.test = 'this is calendar page';
+    // need to use $rootScope to prevent blinking (when?)
+    repository.profiles.getForCurrentUser(function (profiles) {
+        $scope.profiles = profiles;
+        if ($routeParams.profile === undefined) {
+            $scope.currentProfile = profiles[0];
+        }
+        else {
+            $scope.currentProfile = $.grep(profiles, function (p) { return p.Id == $routeParams.profile; })[0];
+        }
+    });
+
+    if ($routeParams.month === undefined) {
+        var now = new Date();
+        $routeParams.year = now.getFullYear();
+        $routeParams.month = now.getMonth() + 1;
+    }
+    $scope.year = $routeParams.year;
+    $scope.month = $routeParams.month;
+    var monthNames = ['Січень', 'Лютий', 'Березень', 'Квітень', 'Травень', 'Червень', 'Липень', 'Серпень', 'Вересень', 'Жовтень', 'Листопад', 'Грудень'];
+    $scope.monthName = monthNames[$routeParams.month - 1];
+
+
 });
