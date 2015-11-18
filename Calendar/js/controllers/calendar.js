a2c62ba356749d84bdc1b1b88e2ec4aeacc8f8e6
Oleg Pylypchak <liole.plo@gmail.com>
2015-11-18 16:51:20 +0200

    Calendar view with links

--- Calendar/js/controllers/calendar.js
+++ Calendar/js/controllers/calendar.js
@@ -22,5 +22,29 @@
     var monthNames = ['Січень', 'Лютий', 'Березень', 'Квітень', 'Травень', 'Червень', 'Липень', 'Серпень', 'Вересень', 'Жовтень', 'Листопад', 'Грудень'];
     $scope.monthName = monthNames[$routeParams.month - 1];
 
+    var daysInMonth = function (year, month) {
+        var date = new Date(year, month, -1);
+        return date.getDate() + 1;
+    }
+    var begin = new Date($scope.year, $scope.month - 1, 1);
+    var dayOfWeek = (begin.getDay() + 6) % 7;
+    begin.setDate(1 - dayOfWeek);
+    Date.prototype.addDays = function (n) {
+        this.setDate(this.getDate() + n);
+    }
+
+    $scope.daysOfWeek = ['ПН', 'ВТ', 'СР', 'ЧТ', 'ПТ', 'СБ', 'НД'];
+   // $scope.daysOfWeek = ['Понеділок', 'Вівторок', 'Середа', 'Четвер', 'П\'ятниця', 'Субота', 'Неділя'];
+    var days = [];
+    var nextMonth = $scope.month % 12;
+    for (var day = begin; day.getMonth() != nextMonth || day.getDay() != 1; day.addDays(1)) {
+        if (day.getDay() == 1)
+            days.push([]);
+        days[days.length - 1].push({
+            day: day.getDate(),
+            currMonth: day.getMonth() == $scope.month-1
+        });
+    }
+    $scope.days = days;
 
 });
