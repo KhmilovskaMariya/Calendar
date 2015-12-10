ec56ede5fb11407728265cf1a71c7ba8e93baed7
Oleg Pylypchak <liole.plo@gmail.com>
2015-12-10 14:45:53 +0200

    Registration link, comments

--- Calendar/js/controllers/login.js
+++ Calendar/js/controllers/login.js
@@ -1,11 +1,14 @@
-﻿angular.module('calendar')
+﻿// Mariya Khmilovska
+angular.module('calendar')
 .controller('login', function ($scope, $http, repository) {
 
     $scope.login = function () {
         repository.user.login($scope.phone, $scope.password,
             function (data) {
+                // if login successful redirect to calendar page
                 window.location.hash = '#/calendar';
             }, function (data) {
+                // if not, show error message
                 $scope.errorPhone = (data.error_description == 'username');
                 $scope.errorPassword = (data.error_description == 'password');
             })
